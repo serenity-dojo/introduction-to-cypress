596f37c52dd56af9d92edbfbffb0e1e831d9b894
John Ferguson Smart <john.smart@wakaleo.com>
2021-03-05 16:48:05 +0000

    Lesson 1

---
+++ cypress-lesson-1/cypress/integration/adding-todos.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('adding a todo item to the todo list', () => {
+
+    it('should add a new item to the list', () => {
+
+        cy.visit('https://todomvc.com/examples/angularjs/#/')
+
+        cy.get('.new-todo').type('Feed the cats{enter}')
+
+        cy.contains('.todo-list li','Feed the cats').should('be.visible')
+    
+    })    
+})
